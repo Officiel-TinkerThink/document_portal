e1ad290d4c85669929c6c7c6974f7ec477662e6a
bornok <bornouksyn497@gmail.com>
2025-08-09 17:01:33 +0700

    document ingestion multidocs

--- src/single_document_chat/retrieval.py
+++ src/single_document_chat/retrieval.py
@@ -1,53 +1,104 @@
 import sys
 import os
+import streamlit as st
 from dotenv import load_dotenv
 from langchain_core.chat_history import BaseChatMessageHistory
 from langchain_community.chat_message_histories import ChatMessageHistory
 from langchain_community.vectorstores import FAISS
 from langchain_core.runnables.history import RunnableWithMessageHistory
 from langchain.chains import create_history_aware_retriever, create_retrieval_chain
+from langchain.chains.combine_documents import create_stuff_documents_chain
 from logger.custom_logger import CustomLogger
 from exception.custom_exception import DocumentPortalException
-from utils.model_loader import ModelLoader, PromptType
+from utils.model_loader import ModelLoader
+from model.models import PromptType
 from prompt.prompt_library import PROMPT_REGISTRY
-
-
+load_dotenv()
 
 
 class ConversationalRAG:
     def __init__(self, session_id: str, retriever) -> None:
+        self.log = CustomLogger().get_logger(__name__)
+        self.session_id = session_id
+        self.retriever = retriever
+
         try:
-            self.log = CustomLogger().get_logger(__name__)
-            self.session_id = session_id
-            self.retriever = retriever
+            self.llm = self._load_llm()
+            self.contextualize_prompt = PROMPT_REGISTRY[PromptType.CONTEXTUALIZE_QUESTION.value]
+            self.qa_prompt = PROMPT_REGISTRY[PromptType.CONTEXT_QA.value]
+
+            self.history_aware_retriever = create_history_aware_retriever(
+                self.llm, self.retriever, self.contextualize_prompt   
+            )
+            self.log.info("Create history aware retriever", session_id=self.session_id)
+            
+            self.qa_chain = create_stuff_documents_chain(self.llm, self.qa_prompt)
+            self.rag_chain = create_retrieval_chain(self.history_aware_retriever, self.qa_chain)
+            self.log.info("Create QA chain", session_id=self.session_id)
+
+            self.chain = RunnableWithMessageHistory(
+                self.rag_chain,
+                self._get_session_history,
+                input_messages_key="input",
+                history_messages_key="chat_history",
+                output_messages_key="answer"
+            )
+            self.log.info("Wrapped chain with message history", session_id=self.session_id)
+
         except Exception as e:
             self.log.error("Error initializing ConversationalRAG", error=str(e))
             raise DocumentPortalException("Error initializing ConversationalRAG", sys)
     
     def _load_llm(self):
         try:
-            pass
+            llm = ModelLoader().load_llm()
+            self.log.info("LLM loaded succefully", class_name=llm.__class__.__name__)
+            return llm
         except Exception as e:
             self.log.error("Error loading LLM via ModelLoader", error=str(e))
             raise DocumentPortalException("Error loading LLM", sys)
     
     def _get_session_history(self, session_id: str):
         try:
-            pass
+            if "store" not in st.session_state:
+                st.session_state.store = {}
+
+            if session_id not in st.session_state.store:
+                st.session_state.store[session_id] = ChatMessageHistory()
+                self.log.info("New chat session history created", session_id=session_id)
+
+            return st.session_state.store[session_id]
         except Exception as e:
             self.log.error("Error getting session history", session_id=session_id,error=str(e))
             raise DocumentPortalException("Error getting session history", sys)
 
-    def load_retriever_from_faiss(self):
+    def load_retriever_from_faiss(self, index_path: str):
         try:
-            pass
+            embeddings = ModelLoader().load_embeddings()
+            if not os.path.isdir(index_path):
+                raise FileNotFoundError(f"FAISS index not found: {index_path}")
+
+            vectorstore = FAISS.load_local(index_path, embeddings)
+            self.log.info("Loaded retriever from FAISS index", index_path=index_path)
+            return vectorstore.as_retriever(search_type="similarity",search_kwargs={"k": 3})
+            
         except Exception as e:
             self.log.error("Error loading retriever from FAISS", error=str(e))
             raise DocumentPortalException("Error loading retriever from FAISS", sys)
 
-    def invoke(self):
+    def invoke(self, user_input: str) -> str:
         try:
-            pass
+            response = self.chain.invoke(
+                {"input": user_input},
+                config = {"configurable": {"session_id": self.session_id}}
+            )
+            answer = response.get("answer", "No answer.")
+
+            if not answer:
+                self.log.warning("No answer received", session_id=self.session_id)
+                
+            self.log.info("Chain invoked succesfully", session_id=self.session_id, user_input=user_input, answer_preview=answer[:50])
+            return answer
         except Exception as e:
             self.log.error("Error invoking ConversationalRAG", error=str(e), session_id=self.session_id)
             raise DocumentPortalException("Error invoking ConversationalRAG", sys)
