6dcd299cf11ed422a587c73f07994a47d76e5a53
bornok <bornouksyn497@gmail.com>
2025-08-20 21:20:13 +0700

    updating config for google model

--- api/main.py
+++ api/main.py
@@ -139,8 +139,6 @@ async def chat_query(
         raise HTTPException(status_code=500, detail=f"Query failed: {e}")
 
 
-
-
 # command for executing the fast api
 # uvicorn api.main:app --reload    
 #uvicorn api.main:app --host 0.0.0.0 --port 8080 --reload
